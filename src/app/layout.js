6f46fc443efe6cf31f9fdc280998dfb7cb770d1a
Andres Rosero Velasquez <andresmedia84@gmail.com>
2024-08-05 17:35:26 -0500

    style: font installed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
-import { Inter } from 'next/font/google'
+import classNames from 'classnames'
+import localFont from 'next/font/local'
 
 import '@/app/globals.css'
 
 export const dynamic = 'force-dynamic'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: {
@@ -14,10 +13,59 @@ export const metadata = {
   description: 'Game Master',
 }
 
+const nextFont = localFont({
+  display: 'swap',
+  fallback: ['ui-monospace', 'monospace'],
+  src: [
+    {
+      path: '../fonts/UbuntuSansMono/Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/RegularItalic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/UbuntuSansMono/BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+})
+
 export default function RootLayout({ children }) {
   return (
     <html lang='es'>
-      <body className={inter.className}>{children}</body>
+      <body className={classNames(nextFont.className, 'text-lg')}>
+        {children}
+      </body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,28 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/icons/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     screens: {
       xs: '360px',
       ...defaultTheme.screens,
     },
-    extend: {},
+    extend: {
+      fontSize: {
+        lg: ['1.125rem', '1.5rem'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
-    themes: ['emerald', 'dim', 'synthwave'],
-    darkTheme: 'dim',
     base: true,
     styled: true,
     utils: true,
-    prefix: '',
     logs: true,
+    prefix: '',
     themeRoot: ':root',
+    darkTheme: 'dim',
+    themes: ['emerald', 'dim', 'synthwave'],
   },
 }
