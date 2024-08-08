29ca9f2e6dfeec053e58dab0890bb6fa670c07b9
Andres Rosero Velasquez <andresmedia84@gmail.com>
2024-08-08 18:44:07 -0500

    clean: files cleaned

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -8,18 +8,8 @@ export default function App() {
   return (
     <main className='px-5'>
       <header>
-        <h1>{`PÁGINA PRINCIPAL`}</h1>
+        <h1>{`WORK IN PROGRESS`}</h1>
       </header>
-
-      <section>
-        <p>{`Primer párrafo del post. Este párrafo introduce el tema del post.`}</p>
-        <p>
-          {`Segundo párrafo del post. Aquí se desarrolla el contenido principal.`}
-        </p>
-        <p>
-          {`Tercer párrafo del post. Este párrafo concluye el post y ofrece un resumen o llamada a la acción.`}
-        </p>
-      </section>
     </main>
   )
 }
